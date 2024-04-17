432c243b6d2d9573809b4d08b233fc040a05d1a8
MASilva139 <mariosva139@gmail.com>
2024-04-17 17:43:49 -0600

    Proyecto03-01

---
+++ Proyectos/P03/P03_p01.py
@@ -0,0 +1,202 @@
+from streamlit_option_menu import option_menu
+from streamlit_extras.stylable_container import stylable_container as stycont
+from streamlit.components.v1 import html
+import streamlit as st
+import pandas as pd
+
+def app():
+    with open('Proyectos/P03/form01.css') as f:
+        css = f.read()
+
+    # Añade tu CSS
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+    
+    st.title('Proyecto 02: Predicción de COVID19')
+    
+    #########################################################
+    ##                       javascript                    ##
+    #########################################################
+    # Define tu JavaScript
+    # bjs = """
+    # function handleClick() {
+    #     // Obtén una referencia a los botones de radio y al elemento de texto
+    #     var radios = document.getElementsByClassName('miRadio');
+    #     var texto = document.getElementById('texto');
+
+    #     // Cambia el color de fondo y el color del texto del botón de radio cuando se selecciona
+    #     for (var i = 0; i < radios.length; i++) {
+    #         radios[i].style.backgroundColor = 'green';
+    #         radios[i].style.color = 'white';
+
+    #         // Muestra el texto correspondiente
+    #         if (radios[i].checked) {
+    #             if (radios[i].value == 'Proyecto') {
+    #                 texto.innerHTML = '<h2>Proyecto</h2>';
+    #             } else if (radios[i].value == 'Referencias') {
+    #                 texto.innerHTML = '<h2>Referencias</h2>';
+    #             }
+    #         }
+    #     }
+    # }
+
+    # // Agrega un manejador de eventos para el evento 'click' a cada botón de radio
+    # window.onload = function() {
+    #     var radios = document.getElementsByClassName('miRadio');
+    #     for (var i = 0; i < radios.length; i++) {
+    #         radios[i].addEventListener('click', handleClick);
+    #     }
+    # };
+    # """
+    # # Define tu HTML para los botones de radio
+    # html_radio = """
+    # <input type="radio" id="proyecto" name="miRadio" class="miRadio" value="Proyecto">
+    # <label for="proyecto">Proyecto</label><br>
+    # <input type="radio" id="referencias" name="miRadio" class="miRadio" value="Referencias">
+    # <label for="referencias">Referencias</label><br>
+    # <script>{}</script>
+    # """.format(bjs)
+    # # Define tu HTML para el texto
+    # html_texto = """
+    # <div id="texto"></div>
+    # """
+    
+    c1, c2 = st.columns([1,5])
+    with c1:
+        #########################################################
+        ##                       st.radio()                    ##
+        #########################################################
+        
+        opt = st.radio("", ["Proyecto", "Referencias"], label_visibility="collapsed")
+        
+        #########################################################
+        ##                       contenedor                    ##
+        #########################################################
+        
+        # with stycont(
+        #         key="menu-01",
+        #         css_styles="""
+        #         """,
+        #     ):
+        #     opt = option_menu(
+        #         #menu_title='Pages',
+        #         None,
+        #         options=['Proyecto', 'Referencias'], # Nombre de cada pestaña
+        #         icons=['house-fill', 'bezier', 'body-text'], #Iconos de las pestañas
+        #         menu_icon='alt',
+        #         default_index=0, # En este se define la primera página en mostrarse,
+        #         orientation="vertical",
+        #         styles={
+        #             "container": {"padding": "1!important", "background-color":'transparent'},
+        #             "icon":{"color":"white", "font-size":"16px"},
+        #             "nav-link":{"color":"white", "font-size": "13px", "text-align":"center", "margin":"0px","--hover-color":"sepia"},
+        #             "nav-link-selected":{"background-color":"darkolivegreen"}
+        #         }
+        #     )
+        
+        #########################################################
+        ##                       javascript                    ##
+        #########################################################
+        # html(html_radio)
+        
+    with c2: 
+        #########################################################
+        ##                       st.radio                      ##
+        #########################################################
+        if opt == "Proyecto":
+            st.markdown("## Resumen / Caso de estudio")
+            st.write(
+            """
+                En la siguiente práctica se procedió a realizar una aplicación web para graficar los datos de los registros de casos de COVID-19 del año 2020 dados por el Ministerio de Salud y a partir de ello predecir el pico de contagios asumiendo que no se cuentan con los datos futuros; empleando para ello la biblioteca de ``Plotly-Express``, para gráficar el histograma y la curva de ajuste, y el programa de ``GNUPlot``, para los valores de la curva de ajuste. \n
+                Empleando el programa de ``GNUPlot`` se determinaron los valores de las constantes de la ecuación de Distribución Gaussiana $$A$$, $$\mu$$ y $$\sigma$$, realizando un fit sobre los datos de casos positivos, columna 5 del csv; utilizando 69 datos, comenzando desde el día 13 de marzo del 2020, se determinaron que los valores de las constantes son $$A=$$930.848, $$\mu=$$109.684 y $$\sigma=$$23.3855. Se vectorizó la función de Distribución Gaussiana para poder introducir diferentes valores de $$x$$ como una lista y con ello poder graficar la curva de ajuste respecto a los datos de casos positivos registrados. \n
+                Se evaluaron los datos disponibles y se eliminaron aquellos que ocasionaban un sobreajuste al fit, de manera que solamente se utilizaron los valores de los primeros 69 días, debido a que con otros datos el ajuste toma que el pico de casos positivos ya se dio, de manera que los datos solamente decrecerian. De igual manera, se utilizó la distribución Gaussiana, en lugar de la distribución binomial, debido a que al tratarse de datos y no probabilidades, el ajuste que daba la primera de estas era mejor, mientras que con la binomial se hubiese tenido que arreglar la función o los datos para poder realizar el ajuste de la curva.
+            """
+            )
+            st.markdown("## Marco Teórico")
+            
+            d = {'Comando': ['pwd','cd','load [fichero]','clear','exit/quit','plot','splot','replot'], 'Descripción': ['Indica cual es el directorio por defecto.','Cambia el directorio por defecto. El path del directorio se puede indicar en forma absoluta o relativa.','Si [fichero] es un script contiene comandos gnuplot, esta orden ejecuta dichos comandos. Cuando se termina, se vuelve al modo interactivo.','Borra el terminal gráfico.','En la línea de comandos de gnuplot, terminan la ejecución del programa.','Para dibujar curvas planas y gráficos 2D.','Para dibujar superficies.','Para hacer modificaciones de un plot o un splot anterior.']}
+            df = pd.DataFrame(d)
+            
+            # A partir de aquí se escribe para el marco teórico
+            st.write(
+                """
+                ### Distribución Normal
+            
+                También conocida como distribución de Gauss, es una distribución de variable continua, con campo de variación $$(-\infty, \infty)$$. Esta queda especificada por dos parámetros de los que depende su función de densidad y que resultan ser la media $$\mu$$ y la dsviación típica de la distribución $$\sigma$$. Su estudio teórico suele introducirse directamente a partir de su función de densidad.
+                
+                Dado que depende de dos parámetros, $$\mu$$ y $$\sigma$$, el hecho de que una variable $$X$$ se distribuya con una distribución normal de media $$\mu$$ y desviación típica $$\sigma$$ se representa por la siguiente expresión:
+                """
+                r'''
+                $$
+                X \rightarrow N \left[\mu : \sigma\right] \hspace{20pt} \| \hspace{20pt} L(X) \rightarrow N \left[\mu : \sigma\right]
+                $$
+                '''
+                """
+                Por tanto, se tiene que la función de densidad está dada por 
+                """
+                r'''
+                $$
+                f(x) = \frac{1}{\sigma\sqrt{2\pi}}e^{-\frac{1}{2}\left(\frac{x-\mu}{\sigma}\right)^{2}} \hspace{20pt} \forall x \in \Re
+                $$
+                '''
+                """
+                Las características de dicha función de densidad son:
+                """
+                '''
+                1. Si se realiza la primera derivada de la función se tiene que
+                '''
+                r'''
+                $$
+                f'(x) = -\frac{\left(x-\mu\right)}{{\sigma}^{3}\sqrt{2\pi}}e^{-\frac{1}{2}\left(\frac{x-\mu}{\sigma}\right)^{2}} \longrightarrow f'(x) = -\frac{\left(x-\mu\right)}{\sigma^{2}}\cdot f(x)
+                $$
+                '''
+                '''
+                2. Si se realiza la segunda derivada de la función se tiene que
+                '''
+                r'''
+                $$
+                f''(x) = \frac{\left(x-\mu\right)^{2}-\sigma^{2}}{\sigma^{4}}\cdot f(x)
+                $$
+                '''
+                """
+                Al igualar a cero la primera derivada se obtiene que $$f'(x)=0$$ para $$X=\mu$$ y para $$X=\infty$$. Como la segunda derivada en $$X=\mu$$ es negativa, se puede concluir que la función de densidad presenta un máximo en $$X=\mu$$, lo que afir,a que la media es también la moda de la distribución normal.
+                
+                #### Fórmula
+                """
+                r'''
+                $$
+                P_{G}(x)=\frac{1}{\sigma\sqrt{2\pi}}e^{-\frac{1}{2}\left(\frac{x-\mu}{\sigma}\right)^{2}} \longleftrightarrow P_{G}(x)=\frac{1}{\sigma\sqrt{2\pi}} \cdot \exp{\left[-\frac{1}{2}\left(\frac{x-\mu}{\sigma}\right)^{2}\right]}
+                $$
+                '''
+                """
+                Donde $$P_G(x)$$ es la distribución gaussiana. La ecuación empleada en la práctica está dada por:
+                """
+                r'''
+                $$
+                P_{G}(x)=A\cdot e^{-\frac{1}{2}\left(\frac{x-u}{r}\right)^{2}} \longleftrightarrow P_{G}(x)=A\cdot \exp{\left[-\frac{1}{2}\left(\frac{x-u}{r}\right)^{2}\right]}
+                $$
+                '''
+                """
+                #### GNUPlot
+            
+                Es un programa de visualización gráfica de datos científicos. Permite realizar gráficos 2D y 3D de curvas, líneas de nivel y superficies, tanto a partir de funciones como de datos discretos. Este software funciona mediante comandos, que pueden usarse en modo interactivo como escribiendo scripts; es decir, la secuencia de comandos escritos en un fichero.\n
+                Algunos comandos son:
+                """
+            )
+            st.table(df)
+            
+            # if st.button('Ir al inicio'):
+            #     st.write('<meta http-equiv="refresh" content="0">', unsafe_allow_html=True)
+                
+        if opt == "Referencias":
+            st.markdown("## Referencias bibliográficas (marco teórico)")
+            wpages1 = [ 'CEACES, (n.d.). Distribución normal, [PDF]. Recuperado de https://www.uv.es/ceaces/pdf/normal.pdf', "Echevarría, R. (n.d.). Breve Introducción a GNUPlot. Recuperado de https://personal.us.es/echevarria/documentos/APUNTESgnuplot.pdf"]
+            st.write(wpages1)
+            
+            st.markdown("## Referencias bibliográficas (documentación)")
+            wpages2 = ['Anónimo, (n.d.). Streamlit-extras. Recuperado de: https://extras.streamlit.app','KaTeX. (2024). Supported Functions. Recuperado de: https://katex.org/docs/supported.html','Pandas documentation, (2024). pandas.DataFrame. Recuperado de: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html','Streamlit Inc. (n.d.). st.columns. Recuperado de: https://docs.streamlit.io/library/api-reference/layout/st.columns','Streamlit Inc. (n.d.). st.markdown. Recuperado de: https://docs.streamlit.io/library/api-reference/text/st.markdown','Streamlit Inc. (n.d.). st.radio. Recuperado de: https://docs.streamlit.io/library/api-reference/widgets/st.radio','Streamlit Inc. (n.d.). st.set_page_config. Recuperado de: https://docs.streamlit.io/library/api-reference/utilities/st.set_page_config', 'Streamlit Inc. (n.d.). st.slider. Recuperado de: https://docs.streamlit.io/library/api-reference/widgets/st.slider','Streamlit Inc. (n.d.). st.table. Recuperado de: https://docs.streamlit.io/develop/api-reference/data/st.table','Streamlit Inc. (n.d.). st.write. Recuperado de: https://docs.streamlit.io/library/api-reference/write-magic/st.write']
+            st.write(wpages2)
+            
+        #########################################################
+        ##                       javascript                    ##
+        #########################################################
+        # html(html_texto)
---
+++ Proyectos/P03/P03_p02.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import numpy as np
+import math
+
+def app():
+    with open('Proyectos/P03/form01.css') as f:
+        css = f.read()
+
+    # Añade tu CSS
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+    
+    st.title('Distribuciones para la Predicción de COVID19')
+    
+    st.markdown("## **Procedimiento Experimental**")
+    # Sección del procedimiento del proyecto
+    st.write("""
+        En la presente práctica se llevó a cabo el análisis para realizar una predicción con los datos registrados por el Ministerio de Salud, de los casos de COVID-19 en el año 2020. A partir de ello se procedió de la siguiente manera
+    """)
+    
+    expa0 = st.toggle("##### Datos de las constantes GNUPlot (Parte experimental)")
+    if expa0:
+        #fit.log → lin: 2750 → Wed Apr 10 14:54:58 2024
+        st.write(
+            '''
+            1. Se definió la función de la Distribución Gaussiana, $$f(x)$$.
+            2. Se indicaron los valores de las constantes como $$A$$=400, $$u$$=200 y $$r$$=100.
+            3. Empleando el comando ``fit f(x) (...)`` se realizó la gráfica de la función con los datos de casos positivos.
+            4. Se indicó el uso de 69 datos.
+            5. A partir de las iteraciones se determinaron los valores de $$A$$, $$u$$ y $$r$$ que ajustan de mejor manera el fit.
+            '''
+        )
+        
+    expa1 = st.toggle("##### Gráfica de la distribución (parte experimental)")
+    if expa1:
+        st.write(
+            '''
+            1. Se definió, con el comando ``def``, la función de la Distribución Gaussiana, $$P_{G}(x)$$, con los valores de las constantes $$A$$, $$u$$ y $$r$$ previamente obtenidos.
+            2. Se vectorizó la función de la Distribución Gaussiana con ``numpy.vetorize()``, para diferentes valores de $$x$$.
+            3. Se definió un rango de 185 datos con ``numpy.arange``.
+            4. Se definieron los parámetros de la gráfica y la curva del ajuste.
+            5. Se gráfico el ajuste y el histograma con el comando ``streamlit.plotly_chart()``.
+            '''
+        )
+####################################################################
+##                        Gráfica de Plotly                       ##
+####################################################################
+    # Crear pandas con los datos
+    data = pd.read_csv('Proyectos/P02/csv/covid.csv')
+    df = pd.DataFrame(data)
+    
+    # Definir fórmula del fit
+    # Fit desde el 13 de marzo → 80 días (01-junio)
+    def fit(x):
+        A=298.165
+        u=73.5442
+        r=9.04991
+        x = np.array(x, dtype=int)
+        return A*math.exp(-((x-u)/r)**2/2)
+    fit = np.vectorize(fit)
+    
+    value_range = np.arange(100)
+    
+    plot_fit = px.line(x=value_range, y=fit(value_range))
+    plot_fit.update_traces(line_color='#B21914', line_width=2.5)
+    plot_fit.update_layout({'plot_bgcolor':'rgba(0,0,0,0)','paper_bgcolor':'rgba(0,0,0,0)'})
+
+    #Fit 13 de marzo → 69 días
+    def fit_2(x):
+        A=930.848
+        u=109.684
+        r=23.3855
+        x = np.array(x, dtype=int)
+        return A*math.exp(-((x-u)/r)**2/2)
+    fit_2 = np.vectorize(fit_2)
+
+    value_range_2 = np.arange(185)
+
+    plot_fit_2 = px.line(x=value_range_2, y=fit_2(value_range_2))
+    plot_fit_2.update_traces(line_color='#B21914', line_width=2.5)
+    plot_fit_2.update_layout({'plot_bgcolor':'rgba(0,0,0,0)','paper_bgcolor':'rgba(0,0,0,0)'})
+    
+    st.markdown("## **Resultados**")
+    # Sección de los resultados
+    rlist = ['**Gráfica 01**', '**Gráfica 02**', '**Tabla 01**']
+    rlist02 = ['Gráfica utilizando 69 datos, iniciando desde el 13 de marzo del 2020 hasta el 20 de mayo.', 'Gráfica utilizando 80 datos, iniciando desde el 01 de marzo hasta el 01 de junio.', 'Datos de las gráficas.']
+    # Diccionario con los valores de rlist con el valor de cada valor de rlist02
+    dic = {key: i for key, i in zip(rlist,rlist02)}
+    # Imprime cada par en el markdown
+    s = "\n".join([f'- {key}: {i}' for key, i in dic.items()])
+    # for i in rlist:
+    #     s += "- " + i + "\n"
+    st.write(
+        """
+        En la presente sección se presentarán los resultados obtenidos en la presente práctica, de los cuales están divididos en:
+        """
+    )
+    st.markdown(s)
+    
+    c1, c2 = st.columns([1,4])
+    with c1:
+        resultados = st.radio(
+            "**Resultados**", 
+            ["Gráfica 01", "Gráfica 02", "Tabla 01"]
+        )
+        
+    with c2:
+        if resultados == "Gráfica 01":
+            st.markdown("## Gráfica 13 de marzo hasta el 20 de mayo")
+            
+            # c3, c4 = st.columns([6,1.5])
+            # with c3:
+                # Mostrar gráfica de plotly
+            don = st.toggle('Comparar la predicción con los datos reales:')
+
+            if don:
+                plot_fit_2.add_bar(x=df.index, y=df['resultados'], marker_color='#291a4d')
+            else:
+                plot_fit_2.add_bar(x=df.index, y=df['resultados'].iloc[:81], marker_color='#291a4d')
+            st.plotly_chart(plot_fit_2)
+                
+        if resultados == "Gráfica 02":
+            st.markdown("## Gráfica 13 de marzo hasta el 01 de junio")
+            
+            # c3, c4 = st.columns([6,1.5])
+            # with c3:
+            on = st.toggle('Ver la exactitud de la predicción:')
+
+            if on:
+                plot_fit.add_bar(x=df.index, y=df['resultados'], marker_color='#291a4d')
+            else:
+                plot_fit.add_bar(x=df.index, y=df['resultados'].iloc[:81], marker_color='#291a4d')
+            st.plotly_chart(plot_fit)
+            
+        if resultados == "Tabla 01":
+            st.markdown("## Datos de las gráficas")
+            l = np.arange(368)
+            table = pd.DataFrame({'Fechas': df['fecha'], 'Resultados Positivos': df['resultados']})
+            st.table(table)
+            
+    
+    st.markdown("## **Discusión de Resultados**")
+    # Sección de los resultados
+    st.write(
+        """
+        A partir de los datos dados por el Ministerio de Salud, de los casos positivos desde el 13 de marzo del 2020 hasta el 15 de marzo del 2021 (véase apartado ***Tabla 01***), se procede a realizar en las gráficas el histograma presentado en la ***Gráfica 01*** y ***Gráfica 02***. Del mismo modo se realizó una curva de ajuste empleando para ello la distribución gaussiana, con respecto a cierta cantidad de datos del histograma, para predecir un posible pico de contagios de COVID-19; esto es, considerando que se está en el día 69 y 80, después del primer caso, presentados en la ***Gráfica 01*** y ***Gráfica 02***, respectivamente.
+        \n
+        De los fits realizados en ``GNUPlot`` se consideró utilizar el del día 69, después del primer caso de contagio registrado, presentados en los apartados ***Gráfica 01*** y ***Fit 11 GNUPlot***, del apédice. Esto se debe a que en otros días los valores aumentaban o disminuian de manera drástica, como en el caso que se presenta en el ***Fit 13 GNUPlot*** (véase apéndice), donde el valor de $$A$$ cambiaba de 930,848 $$\pm$$ 3038 a 73351,4 $$\pm$$ 1,105e+06. Además, a partir del día 71 los datos tendían a decrecer, y con ello al realizar el ajuste tomando de ese día en adelante hacía que el fit proyectara que el valor máximo ya había sucedido y decreciera en vez de predecir un pico.
+        
+        De igual manera existían datos anteriores que también lograban realizar una proyección de un pico coherente a futuro, pero el utilizar estos ocasionaba que se desecharan
+        un gran número de datos y por lo mismo su predicción podría ser menos fiable. Pero, como se mencionó anteriormente, también existían datos que ocasionaban que el ajuste
+        se disparara a cantidades exageradas, debido a que eran días en donde los datos aumentaban o disminuían de manera drástica, obteniendo de esta manera una proyección que
+        parecía improbable y por ende fue desechada.
+        """
+    )
