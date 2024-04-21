cfc8997afba79dbe754dbd45e7889bc8a3c95e6d
MASilva139 <mariosva139@gmail.com>
2024-04-20 23:29:42 -0600

    Proyecto03-02(p01:01-csv01)

--- Proyectos/P03/P03_p01.py
+++ Proyectos/P03/P03_p01.py
@@ -11,54 +11,7 @@ def app():
     # Añade tu CSS
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
     
-    st.title('Proyecto 02: Predicción de COVID19')
-    
-    #########################################################
-    ##                       javascript                    ##
-    #########################################################
-    # Define tu JavaScript
-    # bjs = """
-    # function handleClick() {
-    #     // Obtén una referencia a los botones de radio y al elemento de texto
-    #     var radios = document.getElementsByClassName('miRadio');
-    #     var texto = document.getElementById('texto');
-
-    #     // Cambia el color de fondo y el color del texto del botón de radio cuando se selecciona
-    #     for (var i = 0; i < radios.length; i++) {
-    #         radios[i].style.backgroundColor = 'green';
-    #         radios[i].style.color = 'white';
-
-    #         // Muestra el texto correspondiente
-    #         if (radios[i].checked) {
-    #             if (radios[i].value == 'Proyecto') {
-    #                 texto.innerHTML = '<h2>Proyecto</h2>';
-    #             } else if (radios[i].value == 'Referencias') {
-    #                 texto.innerHTML = '<h2>Referencias</h2>';
-    #             }
-    #         }
-    #     }
-    # }
-
-    # // Agrega un manejador de eventos para el evento 'click' a cada botón de radio
-    # window.onload = function() {
-    #     var radios = document.getElementsByClassName('miRadio');
-    #     for (var i = 0; i < radios.length; i++) {
-    #         radios[i].addEventListener('click', handleClick);
-    #     }
-    # };
-    # """
-    # # Define tu HTML para los botones de radio
-    # html_radio = """
-    # <input type="radio" id="proyecto" name="miRadio" class="miRadio" value="Proyecto">
-    # <label for="proyecto">Proyecto</label><br>
-    # <input type="radio" id="referencias" name="miRadio" class="miRadio" value="Referencias">
-    # <label for="referencias">Referencias</label><br>
-    # <script>{}</script>
-    # """.format(bjs)
-    # # Define tu HTML para el texto
-    # html_texto = """
-    # <div id="texto"></div>
-    # """
+    st.title('Proyecto 03: Decaimiento de Cesio-137')
     
     c1, c2 = st.columns([1,5])
     with c1:
@@ -67,36 +20,6 @@ def app():
         #########################################################
         
         opt = st.radio("", ["Proyecto", "Referencias"], label_visibility="collapsed")
-        
-        #########################################################
-        ##                       contenedor                    ##
-        #########################################################
-        
-        # with stycont(
-        #         key="menu-01",
-        #         css_styles="""
-        #         """,
-        #     ):
-        #     opt = option_menu(
-        #         #menu_title='Pages',
-        #         None,
-        #         options=['Proyecto', 'Referencias'], # Nombre de cada pestaña
-        #         icons=['house-fill', 'bezier', 'body-text'], #Iconos de las pestañas
-        #         menu_icon='alt',
-        #         default_index=0, # En este se define la primera página en mostrarse,
-        #         orientation="vertical",
-        #         styles={
-        #             "container": {"padding": "1!important", "background-color":'transparent'},
-        #             "icon":{"color":"white", "font-size":"16px"},
-        #             "nav-link":{"color":"white", "font-size": "13px", "text-align":"center", "margin":"0px","--hover-color":"sepia"},
-        #             "nav-link-selected":{"background-color":"darkolivegreen"}
-        #         }
-        #     )
-        
-        #########################################################
-        ##                       javascript                    ##
-        #########################################################
-        # html(html_radio)
         
     with c2: 
         #########################################################
@@ -117,6 +40,73 @@ def app():
             df = pd.DataFrame(d)
             
             # A partir de aquí se escribe para el marco teórico
+            st.write(
+                r"""
+                ### Cesio
+                
+                Es un metal de color blanco argénteo muy reactivo que cristaliza en pequeños octaedros. Se caracteriza por su baja dureza. Tiene el puesto más alto en la serie electromotriz; posee también el punto de fusión más bajo de cualquier otro metal y el más bajo potencial iónico de cualquier elemento. Tiene un único isótopo estable, de masa atómica 133. Químicamente presenta el número de oxidación +1. El cesio se oxida con facilidad y arde fácilmente en presencia del aire a temperatura ambiente. Es soluble en alcohol y ácidos.
+                
+                #### Cesio-137
+                
+                Es un isótopo radiactivo del cesio que se produce principalmente por fisión nuclear. Tiene un periodo de semidesintegración de 30,23 años y decae emitiendo partículas beta a un isómero nuclear metaestable de Bario-137. El Bario-137 tiene un periodo de semidesintegración de 2,65 minutos.
+                
+                ### Decaimiento radioactivo
+                
+                Es un proceso en el que un núcleo inestable se transforma en uno más estable, emitiendo patículas y/o fotones y liberando energía durante el proceso. 
+                Una sustancia que experimenta este fenómeno espotáneamente se denomina sustancia radioactiva. Pueden emitir tres tipos de radiación:
+                - Radiación $$\alpha$$
+                - Radiación $$\beta$$
+                - Radiación $$\gamma$$
+                
+                En general, los núcleos de los distintos elementos no son estables. Emiten espontáneamente partículas cargadas y radiación electromagnética. Este fenómeno se conoce como radiactividad natural.
+                
+                Los núcleos excitados se desexcitan mediante tres tipos de decaimiento: 
+                - $$\alpha$$: consiste en la emisión de helio doblemente ionizados.
+                - $$\beta$$: consiste en la emisión de partículas beta, electrones o positrones.
+                - $$\gamma$$: cuando la desexcitación se lleva a cabo mediante la emisión de radiación electromagnética.
+                
+                La desintegración radiactiva es un fenómeno naturalmente estadístico. Las hipótesis con las cuales se trabaja para el estudio de las desintegraciones radiactivas son:
+                - Dado un intervalo temporal, todos los átomos de una muestra tienen la misma probabilidad de desintegrarse en dicho intervalo.
+                - La desintegración de un átomo es un evento independiente de la desintegración de los demás átomos de la muestra.
+                - La probabilidad de desintegración de un átomo en un intervalo temporal permanece constante para todo intervalo temporal de la misma duración.
+                
+                Las fluctuaciones en el número de desintegraciones están representadas por una distribución de Poisson
+                """
+                r'''
+                $$
+                P_{P}(x) = \frac{\mu^{x}e^{-\mu}}{x!}
+                $$
+                '''
+                r"""
+                Esta distribución expresa la probabilidad $$P_{P}(x)$$ de que en cierto intervalo se observa un número entero $$x$$ de desintegraciones, siendo $$\mu$$ el número de desintegraciones predicho.
+                
+                ### Contador de Geiger
+                Proximamente agregado :) (posiblemente agregado)
+                
+                ### Prueba $$\chi$$-cuadrado $$\left(\chi^{2}\right)$$
+                Esta prueba puede utilizarse con datos medibles en una escala nominal. La hipótesis nula de la prueba $$\chi^{2}$$ postula una distribución de probabilidad totalmente específicada.
+                
+                Para realizar este contraste se disponen los datos en una tabla de frecuencias. Para cada valor o intervalo de valores se indica la frecuencia absoluta observada o empirica $$(x_{i})$$. A continuación, y suponiendo que la hipótesis nula es cierta, se calculan para cada valor o intervalo de valores la frecuencia absoluta que cabría esperar o frecuencia esperada
+                """
+                r'''
+                $$
+                \Omicron_{i} = n\cdot p_{i}
+                $$
+                '''
+                r"""
+                donde $$n$$ es el tamaño de la muestra y $$p_{i}$$ la probabilidad del $$i$$-ésimo valor o intervalo de valores según la hipótesis nula. La hipótesis nula es cierta si $$n\rightarrow\infty$$, de manera que la prueba $$\chi^{2}$$ se define por
+                """
+                r'''
+                $$
+                \chi^{2} = \sum_{i=1}^{k}\frac{\left(x_{i}-\Omicron_{i}\right)^{2}}{\Omicron_{i}}
+                $$
+                '''
+                r"""
+                Esta ecuación tiene una distribución $$\chi^{2}$$ con $$k-1$$ grados de libertad si $$n$$ es suficientemente grande, es decir, si todas las frecuencias esperadas son mayores que 5.
+                
+                Si existe concordancia perfecta entre las frecuencias observadas y las esperadas el estadístico tomará un valor igual a 0; por el contrario, si existe una gran discrepancia entre estas frecuencias el estadístico tomará un valor grande y, en consecuencia, se rechazará la hipótesis nula. Así pues, la región crítica estará situada en el extremo superior de la distribución $$\chi^{2}$$ con $$k-1$$ grados de libertad.
+                """
+            )
             st.write(
                 """
                 ### Distribución Normal
@@ -183,20 +173,14 @@ def app():
                 """
             )
             st.table(df)
-            
-            # if st.button('Ir al inicio'):
-            #     st.write('<meta http-equiv="refresh" content="0">', unsafe_allow_html=True)
                 
         if opt == "Referencias":
             st.markdown("## Referencias bibliográficas (marco teórico)")
-            wpages1 = [ 'CEACES, (n.d.). Distribución normal, [PDF]. Recuperado de https://www.uv.es/ceaces/pdf/normal.pdf', "Echevarría, R. (n.d.). Breve Introducción a GNUPlot. Recuperado de https://personal.us.es/echevarria/documentos/APUNTESgnuplot.pdf"]
+            wpages1 = ['Universitat de Barcelona. (n.d.) Prueba Chi-cuadrado, [En Línea]. Recuperado de: http://www.ub.edu/aplica_infor/spss/cap5-2.htm','CEACES, (n.d.). Distribución normal, [PDF]. Recuperado de https://www.uv.es/ceaces/pdf/normal.pdf','Ciocci, L., Acuña, G. (2001) Estudio de la estadística del decaimiento radiactivo [PDF]. Buenos Aires. Recuperado de: http://users.df.uba.ar/sgil/labo5_uba/inform/info/pautadas/estadistica_decaimiento_ligia.pdf', "Echevarría, R. (n.d.). Breve Introducción a GNUPlot. Recuperado de https://personal.us.es/echevarria/documentos/APUNTESgnuplot.pdf",'Navarro, F. (2004) La Enciclopedia Vol 4. Madrid: Salvat.','Wikipedia. (2022) Cesio-137 [En línea]. Recuperado de: https://es.wikipedia.org/wiki/Cesio-137']
+            #https://www.ugr.es/~bioestad/guiaspss/practica6/ (prueba chi-cuadrado, universidad de granada)
             st.write(wpages1)
             
             st.markdown("## Referencias bibliográficas (documentación)")
             wpages2 = ['Anónimo, (n.d.). Streamlit-extras. Recuperado de: https://extras.streamlit.app','KaTeX. (2024). Supported Functions. Recuperado de: https://katex.org/docs/supported.html','Pandas documentation, (2024). pandas.DataFrame. Recuperado de: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html','Streamlit Inc. (n.d.). st.columns. Recuperado de: https://docs.streamlit.io/library/api-reference/layout/st.columns','Streamlit Inc. (n.d.). st.markdown. Recuperado de: https://docs.streamlit.io/library/api-reference/text/st.markdown','Streamlit Inc. (n.d.). st.radio. Recuperado de: https://docs.streamlit.io/library/api-reference/widgets/st.radio','Streamlit Inc. (n.d.). st.set_page_config. Recuperado de: https://docs.streamlit.io/library/api-reference/utilities/st.set_page_config', 'Streamlit Inc. (n.d.). st.slider. Recuperado de: https://docs.streamlit.io/library/api-reference/widgets/st.slider','Streamlit Inc. (n.d.). st.table. Recuperado de: https://docs.streamlit.io/develop/api-reference/data/st.table','Streamlit Inc. (n.d.). st.write. Recuperado de: https://docs.streamlit.io/library/api-reference/write-magic/st.write']
             st.write(wpages2)
             
-        #########################################################
-        ##                       javascript                    ##
-        #########################################################
-        # html(html_texto)
