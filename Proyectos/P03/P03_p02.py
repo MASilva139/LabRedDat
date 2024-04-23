b570e21b6ab771872baf7b930f252228f78dc489
MASilva139 <mariosva139@gmail.com>
2024-04-22 23:28:22 -0600

    Proyecto03-08(p02:06)

--- Proyectos/P03/P03_p02.py
+++ Proyectos/P03/P03_p02.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import streamlit as st
 import numpy as np
 import math
+import mpmath as mpm
 
 def app():
     with open('Proyectos/P03/form01.css') as f:
@@ -46,11 +47,11 @@ def app():
 ####################################################################
 ##                    Gráfica de Plotly (Aire)                    ##
 ####################################################################
-    # Definir fórmula del fit para el aire
+    # Definir fórmula del fit (D. Gaussiana) para el aire
     def fit(x):
-        A=63.5733
-        u=2.18871
-        r=1.59884
+        A = 63.5733
+        u = 2.18871
+        r = 1.59884
         x = np.array(x, dtype=int)
         return A*math.exp(-((x-u)/r)**2/2)
     fit = np.vectorize(fit)
@@ -58,46 +59,122 @@ def app():
     # Datos aire
     data = pd.read_csv('Proyectos/P03/csv/muestra_radiacion.csv')
     df = pd.DataFrame(data)
-    value_range = np.arange(-3,df['Aire'].max()+1)
+    value_range = np.arange(0,df['Aire'].max()+1)
     count = df['Aire'].value_counts().reindex(value_range, fill_value=0).reset_index()
-    print(count)
-
+    #print(count)
+    
     plot_fit = px.line(x=value_range, y=fit(value_range))
     plot_fit.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
     plot_fit.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
     plot_fit.add_bar(x=count['Aire'], y=count['count'])
     
+    # Definir fórmula del fit (D. Poisson) para el aire
+    def poisson_ai(x):
+        m = (df['Aire'].sum())/(df['Aire'].count())
+        # x = np.array(x, dtype=int)
+        P_x = ((m**x)*(mpm.exp(-m)))/(mpm.factorial(x))
+        return float(P_x)
+    v_poisson_ai = np.vectorize(poisson_ai)
+        
+    datpossai = pd.DataFrame({'Aire':df['Aire']})
+    datpossai['Pp(x)'] = datpossai['Aire'].apply(v_poisson_ai)
+    dpoissonai = datpossai.round(15).astype(str)
+    #print(dpoisson01)
+    
+    nd = df['Aire'].count()
+    poisson_fitai = px.line(x=value_range, y=(v_poisson_ai(value_range))*(nd))
+    poisson_fitai.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
+    poisson_fitai.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    poisson_fitai.add_bar(x=count['Aire'], y=count['count'])
+    
 ####################################################################
 ##                    Gráfica de Plotly (Cesio)                   ##
 ####################################################################
-    # Definir fórmula del fit para el cesio
+    # Definir fórmula del fit (D. Gaussiana) para el cesio
     def fit2(x):
-        A=5.09287
-        u=442.826
-        r=19.5836
+        A = 5.09274
+        u = 442.826
+        r = 19.5845
         x = np.array(x, dtype=int)
         return A*math.exp(-((x-u)/r)**2/2)
     fit2 = np.vectorize(fit2)
 
     # Datos cesio
-    print(df['Cesio'].min())
+    #print(df['Cesio'].min())
     value_range2 = np.arange(df['Cesio'].min(),df['Cesio'].max()+1)
     count2 = df['Cesio'].value_counts().reindex(value_range2, fill_value=0).reset_index()
-    print(count2)
-    print(value_range2)
+    #print(count2)
+    #print(value_range2)
 
-    plot_fit2 = px.line(x=value_range2, y=fit(value_range2))
+    plot_fit2 = px.line(x=value_range2, y=fit2(value_range2))
     plot_fit2.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
     plot_fit2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
     plot_fit2.add_bar(x=count2['Cesio'], y=count2['count'])
+    
+    # Definir fórmula del fit (D. Poisson) para el Cesio-137 (1/1)
+    def poisson_ce01(x):
+        m = (df['Cesio'].sum())/(df['Cesio'].count())
+        # x = np.array(x, dtype=int)
+        P_x = ((m**x)*(mpm.exp(-m)))/(mpm.factorial(x))
+        return float(P_x)
+    v_poisson_ce01 = np.vectorize(poisson_ce01)
+        
+    datposs01 = pd.DataFrame({'Cesio':df['Cesio']})
+    datposs01['Pp(x)'] = datposs01['Cesio'].apply(v_poisson_ce01)
+    dpoisson01 = datposs01.round(15).astype(str)
+    #print(dpoisson01)
+    
+    nd01 = df['Cesio'].count()
+    poisson_fit1 = px.line(x=value_range2, y=(v_poisson_ce01(value_range2))*(nd01))
+    poisson_fit1.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
+    poisson_fit1.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    poisson_fit1.add_bar(x=count2['Cesio'], y=count2['count'])
+    
+    #-----------------------------
+    def fit2_1(x):
+        A = 25.382
+        u = 439.84
+        r = 19.6525
+        x = np.array(x, dtype=int)
+        return A*math.exp(-((x-u)/r)**2/2)
+    fit2_1 = np.vectorize(fit2_1)
+    
+    group = np.arange(350, 505, 5)
+    cesio_cut = df.groupby(pd.cut(df['Cesio'], group))['Cesio'].count()
+    #print(cesio_cut)
+    data_c = pd.read_csv('Proyectos/P03/csv/Cesio01.csv')
+    dfd = pd.DataFrame(data_c)
+
+    plot_fit2_1 = px.line(x=group, y=fit2_1(group))
+    plot_fit2_1.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
+    plot_fit2_1.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    plot_fit2_1.add_bar(x=group, y=dfd['count'])
+    
+    # Definir fórmula del fit (D. Poisson) para el Cesio-137 (5/5)
+    def poisson_ce02(x):
+        m = (dfd['Cesio'].sum())/(dfd['Cesio'].count())
+        # x = np.array(x, dtype=int)
+        P_x = ((m**x)*(mpm.exp(-m)))/(mpm.factorial(x))
+        return float(P_x)
+    v_poisson_ce02 = np.vectorize(poisson_ce02)
+        
+    datposs02 = pd.DataFrame({'Cesio':dfd['Cesio']})
+    datposs02['Pp(x)'] = datposs02['Cesio'].apply(v_poisson_ce02)
+    dpoisson01 = datposs02.round(15).astype(str)
+    
+    nd02 = dfd['Cesio'].count()
+    poisson_fit2 = px.line(x=group, y=(v_poisson_ce02(group))*(nd02))
+    poisson_fit2.update_traces(line_color='#B21914', line_width=2.5, line_shape='spline')
+    poisson_fit2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    poisson_fit2.add_bar(x=group, y=dfd['count'])
     
 ####################################################################
 ##                      Apartado de Resultados                    ##
 ####################################################################
     st.markdown("## **Resultados**")
     # Sección de los resultados
-    rlist = ['**Gráfica 01**', '**Gráfica 02**', '**Tabla 01**']
-    rlist02 = ['Gráfica del Cesio-137.', 'Gráfica del Aire.', 'Datos de las gráficas.']
+    rlist = ['**Gráfica 01**', '**Gráfica 02**', '**Gráfica 03**', '**Tabla 01**']
+    rlist02 = ['Distribución Gaussiana del Aire.', 'Distribución del Cesio-137.', 'Distribución del Cesio-137, datos agrupados de 5 en 5.', 'Datos de las gráficas.']
     # Diccionario con los valores de rlist con el valor de cada valor de rlist02
     dic = {key: i for key, i in zip(rlist,rlist02)}
     # Imprime cada par en el markdown
@@ -115,24 +192,42 @@ def app():
     with c1:
         resultados = st.radio(
             "**Resultados**", 
-            ["Gráfica 01", "Gráfica 02", "Tabla 01"]
+            ["Gráfica 01", "Gráfica 02", "Gráfica 03", "Tabla 01"]
         )
         
     with c2:
         if resultados == "Gráfica 01":
-            st.markdown("## Gráfica del decaimiento radiactivo del Cesio-137")
-            
+            st.markdown("## Distribución del decaimiento radiactivo del Aire")
+            st.markdown("### Distribución Gaussiana")
             # c3, c4 = st.columns([6,1.5])
             # with c3:
             # Mostrar gráfica de plotly
             st.plotly_chart(plot_fit)
+            
+            st.markdown("### Distribución de Poisson")
+            st.plotly_chart(poisson_fitai)
                 
         if resultados == "Gráfica 02":
-            st.markdown("## Gráfica del decaimiento radiactivo del Aire")
+            st.markdown("## Distribución del decaimiento radiactivo del Cesio-137")
             
+            st.markdown("### Distribución Gaussiana")
             # c3, c4 = st.columns([6,1.5])
             # with c3:
             st.plotly_chart(plot_fit2)
+            
+            st.markdown("### Distribución de Poisson")
+            st.plotly_chart(poisson_fit1)
+            
+        if resultados == "Gráfica 03":
+            st.markdown("## Distribución del decaimiento radiactivo del Cesio-137, datos agrupados de 5 en 5")
+            
+            st.markdown("### Distribución Gaussiana")
+            # c3, c4 = st.columns([6,1.5])
+            # with c3:
+            st.plotly_chart(plot_fit2_1)
+            
+            st.markdown("### Distribución de Poisson")
+            st.plotly_chart(poisson_fit2)
             
         if resultados == "Tabla 01":
             st.markdown("## Datos de las gráficas")
